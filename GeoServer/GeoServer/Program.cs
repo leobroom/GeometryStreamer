e0b245f989097e35ff6b07b1f2227a2a00ea51b3
Leon Brohmann <leon.brohmann@tu-braunschweig.de>
2019-08-03 12:47:03 +0200

    new Start

---
+++ GeoServer/GeoServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeoServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
